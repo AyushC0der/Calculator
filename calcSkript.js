364c67d48a8756e8262a8cfbd02134b016b96a4f
Ayush Singh <toayush0451@gmail.com>
2025-02-16 01:46:36 +0530

    sound is not working error
    the sound error :(

--- calcSkript.js
+++ calcSkript.js
@@ -1,7 +1,8 @@
 var textbox = [];
 var num1 = 0;
 var num2 = 0;
-
+var bs1 = new Audio("/butt1.mp3");
+var bs2 = new Audio("/butt2.mp3");
 let oper;
 
 let broken = false;
@@ -13,14 +14,17 @@ var butt = document.querySelector("input").value;
 
 function Operator(opr)
 {
+    bs1.play();
     txt.style.color = "black";
     broken = true;
     textbox = [];
     txt.textContent = "0";
     oper = opr;
+    bs1.currentTime = 0;
 }
 
 function Number(num) {
+    bs1.play();
     txt.style.color = "black";
     textbox.push(num);
 
@@ -54,15 +58,19 @@ function Number(num) {
     console.log(num2);
 
     backPr = false;
+    bs1.currentTime = 0;
 }
 
 function BACK()
 {
+    bs2.play();
     txt.style.color = "black";
     backPr = true;
+    bs2.currentTime = 0;
 }
 function EQUAL()
 {   
+    bs2.play();
     txt.style.color = "black";
     var result;
     console.log("num1: " + String(num1));
@@ -93,9 +101,11 @@ function EQUAL()
     broken = false;
     textbox = [];
     oper = "";
+    bs2.currentTime = 0;
 }
 
 function CLEAR(){
+    bs1.play();
     txt.style.color = "black";
     txt.textContent = "0";
     num1 = 0;
@@ -103,5 +113,7 @@ function CLEAR(){
     broken = false;
     textbox = [];
     oper = "";
+
+    bs1.currentTime = 0;
 
 }
